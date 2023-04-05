da1c4b501a530c4abe099e3e390a4302b5361ad7
Carlos Zabala <cazabalab@unal.edu.co>
2023-04-04 23:53:09 -0500

    segundo commit creacion Select

--- src/main/java/Modelo/Estudiante.java
+++ src/main/java/Modelo/Estudiante.java
@@ -3,6 +3,81 @@ package Modelo;
 
 public class Estudiante {
     
+    private int idEstudiante;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+    private double edad;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(int idEstudiante, String nombre, String apellido, String telefono, String email, double edad) {
+        this.idEstudiante = idEstudiante;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+        this.edad = edad;
+    }
+
+    public Estudiante(String nombre, String apellido, String telefono, String email, double edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+        this.edad = edad;
+    }
+
+    public int getIdEstudiante() {
+        return idEstudiante;
+    }
+
+    public void setIdEstudiante(int idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getEdad() {
+        return edad;
+    }
+
+    public void setEdad(double edad) {
+        this.edad = edad;
+    }
+    
     
     
 }
--- src/main/java/Modelo/EstudianteDao.java
+++ src/main/java/Modelo/EstudianteDao.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Modelo;
 
-/**
- *
- * @author carlo
- */
-public class EstudianteDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstudianteDao extends Conexion {
+    private static final String SQL_SELECT = 
+            "SELECT id_estudiante, nombre, apellido, email, telefono, edad FROM estudiante";
+    private static final String SQL_SELECT_BY_ID =
+            "SELECT id_estudiante, nombre, apellido, email, telefono, edad FROM estudiante WHERE id_estudiante = ? ";
+    private static final String SQL_INSERT =
+            "INSERT INTO estudiante (nombre, apellido, email, telefono, edad) VALUES (?,?,?,?,?)";           
+    private static final String SQL_UPDATE =
+            "UPDATE estudiante SET nombre=?, apellido=?, email=?, telefono=?, edad=? WHERE id_estudiante =?";
+    private static final String SQL_DELETE =
+            "DELETE FROM estudiante WHERE id_estudiante=?";
+    
+    
+    
+    public List<Estudiante> Listar(){
+                
+        List<Estudiante> estudiantes = new ArrayList<>();
+        
+        try {
+            Connection con = Conexion.getConnection();
+            PreparedStatement st = con.prepareStatement(SQL_SELECT);
+            ResultSet rs = st.executeQuery();
+            while(rs.next()){
+                int idEstudiante = rs.getInt("id_estudiante");
+                String nombre = rs.getString("nombre");
+                String apellido = rs.getString("apellido");
+                String telefono = rs.getString("telefono");
+                String email = rs.getString("email");
+                double edad = rs.getDouble("edad");
+                
+                Estudiante estudiante = new Estudiante(idEstudiante,nombre,apellido,telefono,email,edad);
+                estudiantes.add(estudiante);
+            }
+        } catch (Exception e) {
+            System.out.println("Error al listar los estudiantes: "+e.getMessage());
+        }
+        
+        return estudiantes;
+    }
+    
     
 }
